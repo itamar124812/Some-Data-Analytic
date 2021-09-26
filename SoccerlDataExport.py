ca0e93b579ab0a285b584a69c15056e88d935a0f
itamar124812 <itamar124812@gmail.com>
2021-09-27 01:00:21 +0300

    Uploud some changes

--- SoccerlDataExport.py
+++ SoccerlDataExport.py
@@ -1,8 +1,13 @@
 import csv
 import os
 import re
-def WrithListToCsvFile(linkedList,filename="new_csv_file"):
+import random
+import matplotlib.pyplot as plt
+import pandas as pd
+def WrithListToCsvFile(linkedList,filename="new_csv_file",Season=""):
     newpath = r'C:\Users\USER\NewDoc' 
+    if(re.match(r"\d{4,4}_\d{4,4}",Season)):
+         newpath+=str.format(r'\{}',Season)    
     if not os.path.exists(newpath):
      os.makedirs(newpath)
     f=open(newpath+ str.format(r'\{}',filename) +".csv",'w') 
@@ -100,12 +105,67 @@ def buildTableFromCsv(path,gamesNumber=38):
         lis[9]=lis[6]/gamesNumber
         lis[10]=lis[5]/lis[1]
     table.sort(key=takeSecond,reverse=True)
-    table.insert(0,["Team","Points","wins","Losess","Drew","Goals Scored","Goals Against","Goal Difference","Goals per game","Goals Against per game","Goals Against points"])
+    table.insert(0,["Team","Points","wins","Losess","Drew","Goals Scored","Goals Against","Goal Difference","Goals per game","Goals Against per game","Goals per point"])
     return table
-
-  
+def WriteAllSeason():
+  a_directory = r"C:\Users\USER\data\data"
+  a=2009
+  for filename in os.listdir(a_directory):
+    filepath = os.path.join(a_directory, filename)
+    WrithListToCsvFile(buildTableFromCsv(filepath,34),str.format("GermanLeageTable-{}_{}",a%100,(a+1)%100),str.format("{}_{}",a,a+1))
+    a+=1
+#WrithListToCsvFile(HomeAwayDataExport(r"C:\Users\USER\Downloads\D1 (1).csv"),str.format("GermanLeageHomeAwayStats-{}_{}",20,21),str.format("{}_{}",2020,2021))
+def buildNeuralNetwork(exeptedOutput,**inputs):
+    weights=[]
+    for i in range(0,len(input)):
+      weights.append(random.random())
     
-                
+def R(X,Y):
+  aveX=sum(X)/len(X) 
+  aveY=sum(Y)/len(Y) 
+  A=0
+  B=sum(map(lambda x:(x-aveX)**2,X))
+  C=sum(map(lambda y:(y-aveY)**2,Y))
+  for i in range(len(Y)):
+   A+=(X[i]-aveX)*(Y[i]-aveY)
+  return A/((B*C)**(1/2))
+def ALinearRegression(X,Y):
+  aveX=sum(X)/len(X) 
+  aveY=sum(Y)/len(Y) 
+  A=0
+  B=0
+  for i in range(len(Y)):
+   A+=X[i]*(Y[i]-aveY)
+   B+=X[i]*(X[i]-aveX)
+  return [float(A)/B,aveY-aveX*float(A)/B]
+with open(r"C:\Users\USER\Downloads\D1 (2).csv","r")  as f:
+    R_HomeFauls_HomeBet=-0.045477074114620236
+    reader=csv.reader(f)
+    L=[]
+    P=[]
+    i=0
+    for line in reader:
+          if(i!=0):
+           L.append(float(line[5]))
+           P.append(float(line[23]))
+          i+=1
+   # L.remove("HF")
+    #P.remove("B365H")
+    print(ALinearRegression(L,P))
+    plt.plot(P, L, color = 'g', linestyle = 'dashed',
+         marker = 'o',label = "BetRelationGoals")
+    u=0
+    for x in range(0,len(L)-1):
+     u+=P[x]
+    print(u/len(P))
+plt.xticks(rotation = 25)
+plt.xlabel('Bet')
+plt.ylabel('Goals')
+plt.title('BetRelationGoals', fontsize = 20)
+plt.grid()
+plt.legend()
+plt.show()
+
 
 
 
